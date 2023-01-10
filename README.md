fcb3cf39b8d2b8273e5093b470998a5efa1694cd
ashley <vothuykg123@gmail.com>
2023-01-10 22:58:42 +0700

    logic

---
+++ README.md
@@ -0,0 +1,2 @@
+# Colorgame
+Ehehe
---
+++ app.js
@@ -0,0 +1,138 @@
+
+
+var easyColors = [
+    randomColorToGridLayout(),
+    randomColorToGridLayout(),
+    randomColorToGridLayout(),
+    randomColorToGridLayout(),
+    randomColorToGridLayout(),
+    randomColorToGridLayout()];
+
+var hardColors = [
+    randomColorToGridLayout(),
+    randomColorToGridLayout(),
+    randomColorToGridLayout(),
+    randomColorToGridLayout(),
+    randomColorToGridLayout(),
+    randomColorToGridLayout(),
+    randomColorToGridLayout(),
+    randomColorToGridLayout(),
+    randomColorToGridLayout()];
+
+var panel = document.getElementById("panel");
+var result = document.getElementById("result");
+var hard = document.getElementById("hard");
+var easy = document.getElementById("easy");
+var squares = document.querySelectorAll(".square")
+var hardSquares = document.querySelectorAll(".hardSquare")
+var btnHard = document.getElementById("hardLevel")
+var btnEasy = document.getElementById("easyLevel")
+
+var questionColor = random();
+var question = document.getElementById(
+    "question");
+var getColors = 3;
+getNameOfPlayer = prompt("Who are u?");
+result.innerHTML = "Let's go my sweetie " + getNameOfPlayer;
+
+btnHard.addEventListener("click", function () {
+    hardLevel()
+})
+btnEasy.addEventListener("click", function () {
+    easyLevel()
+})
+//Random easyColors and push to grid layout
+function randomColorToGridLayout() {
+
+    var red = Math.floor(Math.random() * 256);
+    var green = Math.floor(Math.random() * 256);
+    var blue = Math.floor(Math.random() * 256);
+    return "rgb" + "(" + red + ", " + green + ", " + blue + ")";
+}
+
+//random question and push to grid
+function random() {
+    var random = Math.floor(Math.random() * 6);
+    return easyColors[random];
+}
+function randomForHardLevel() {
+    var random = Math.floor(Math.random() * 9);
+    return hardColors[random];
+}
+
+
+
+function reset() {
+    location.reload();
+}
+
+function easyLevel() {
+    document.getElementById("easy").style.visibility = "visible";
+    question.textContent = questionColor;
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = easyColors[i];
+        squares[i].addEventListener("click", function () {
+            var clickedColor = this.style.backgroundColor;
+            if (clickedColor === questionColor) {
+                result.innerHTML = "That right";
+                panel.style.backgroundColor = questionColor;
+                for (var i = 0; i < squares.length; i++) {
+                    squares[i].style.backgroundColor = questionColor;
+                }
+
+            }
+            else {
+                this.style.backgroundColor = "rgb(45, 47, 61)";
+                result.innerHTML = "Wrong answer my kid :))";
+            }
+        })
+    }
+}
+
+function hardLevel() {
+    document.getElementById("hard").style.visibility = "visible";
+    document.getElementById("easy").style.visibility = "visible";
+    var questionColorForHardLevel = randomForHardLevel();
+    question.textContent = questionColorForHardLevel;
+    //set color for hard squares
+    for (var i = 0; i < hardSquares.length; i++) {
+        hardSquares[i].style.backgroundColor = hardColors[i];
+        hardSquares[i].addEventListener("click", function () {
+            var clickedColor = this.style.backgroundColor;
+            if (clickedColor === questionColorForHardLevel) {
+                result.innerHTML = "That right";
+                panel.style.backgroundColor = questionColorForHardLevel;
+                for (var i = 0; i < squares.length; i++) {
+                    squares[i].style.backgroundColor = questionColorForHardLevel;
+                }
+
+            }
+            else {
+                this.style.backgroundColor = "rgb(45, 47, 61)";
+                result.innerHTML = "Wrong answer my kid :))";
+            }
+        })
+    }
+    //set color for easy squares
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = hardColors[getColors];
+        getColors++;
+        squares[i].addEventListener("click", function () {
+            var clickedColor = this.style.backgroundColor;
+            if (clickedColor === questionColorForHardLevel) {
+                result.innerHTML = "That right";
+                panel.style.backgroundColor = questionColorForHardLevel;
+                for (var i = 0; i < squares.length; i++) {
+                    squares[i].style.backgroundColor = questionColorForHardLevel;
+                }
+
+            }
+            else {
+                this.style.backgroundColor = "rgb(45, 47, 61)";
+                result.innerHTML = "Wrong answer my kid :))";
+            }
+        })
+    }
+}
+
+
